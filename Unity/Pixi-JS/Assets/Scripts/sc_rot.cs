148921785d3a3b71cfe9e479d2d78c46ca79c347
desjardins jonathan <jonathan.desjardins@e-artsup.net>
2022-03-23 10:25:24 +0100

    Create Branch

---
+++ Unity/Pixi-JS/Assets/Scripts/sc_rot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_rot : MonoBehaviour
+{
+    Vector3 v3_startray;
+    Vector3 v3_endray;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        v3_startray = Input.mousePosition;
+
+        if(Input.GetKeyDown(0))
+        {
+            Ray r_ray;
+            RaycastHit h_hit;
+
+            
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+
+
+
+        }
+    }
+}
